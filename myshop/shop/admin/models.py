c35da55591b8bc1b53612a3765b307bfbf2a0de7
Mumin8 <alhassanmumin8@gmail.com>
2023-05-24 14:49:43 +0000

    User model for sqlite database

---
+++ myshop/shop/admin/models.py
@@ -0,0 +1,17 @@
+from shop import db, app
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), unique=False, nullable=False)
+    username = db.Column(db.String, unique=True, nullable=False)
+    email = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(180), unique=False, nullable=False)
+    profile = db.Column(db.String(180), unique=True, nullable=False, default='profile.jpg')
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+
+with app.app_context():
+    db.create_all()
