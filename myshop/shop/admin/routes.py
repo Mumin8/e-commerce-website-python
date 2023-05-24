3b86e1acd3d65515144a2d4c789dedfe5efdf920
Mumin8 <alhassanmumin8@gmail.com>
2023-05-24 09:09:05 +0000

    registration form

---
+++ myshop/shop/admin/routes.py
@@ -0,0 +1,18 @@
+from flask import render_template, session, request, redirect, url_for, flash
+from shop import app, db
+from .forms import RegistrationForm
+
+@app.route("/")
+def home():
+    return "Hello, World!"
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm(request.form)
+    if request.method == 'POST' and form.validate():
+        # user = User(form.username.data, form.email.data,
+        #             form.password.data)
+        # db_session.add(user)
+        flash('Thanks for registering')
+        return redirect(url_for('login'))
+    return render_template('admin/register.html', form=form, title="Registration page ")
