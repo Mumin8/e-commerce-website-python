96c426c92aad58538bb1cebe01d90d31bf281e2e
Mumin8 <alhassanmumin8@gmail.com>
2023-05-29 10:03:46 +0000

    init module

--- myshop/shop/__init__.py
+++ myshop/shop/__init__.py
@@ -1,8 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
+
+db = SQLAlchemy()
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///myshop.db"
-db = SQLAlchemy(app)
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///mydb.db'
+app.config['SECRET_KEY'] = 'somegibrishword'
+
+db.init_app(app)
+bcrypt = Bcrypt(app)
 
 from shop.admin import routes
+from shop.products import routes
