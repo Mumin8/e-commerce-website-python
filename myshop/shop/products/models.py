890aea8c8d5c347d9e45688e69cee2fe9be2b043
Mumin8 <alhassanmumin8@gmail.com>
2023-05-31 17:14:50 +0000

    correction

--- myshop/shop/products/models.py
+++ myshop/shop/products/models.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 
 class Addproduct(db.Model):
-    """The model for Addproduct
+    """Defines a  model for Addproduct
        It defines fields for the the product to be added
     """
 
@@ -14,22 +14,22 @@ class Addproduct(db.Model):
     discount = db.Column(db.Integer, default=0)
     stock = db.Column(db.Integer, nullable=False)
     color = db.Column(db.Text, nullable= False)
-    desc = db.Column(db.DateTime, nullable=False)
+    desc = db.Column(db.Text, nullable=False)
     pub_date = db.Column(db.DateTime, nullable=False,
                         default=datetime.utcnow)
 
     brand_id = db.Column(db.Integer, db.ForeignKey('brand.id'), nullable=False)
-    brand = db.relationship('brand', backref=db.backref('brands', lazy=True))
+    brand = db.relationship('Brand', backref=db.backref('brands', lazy=True))
 
     category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=False)
-    category = db.relationship('category', backref=db.backref('categories', lazy=True))
+    category = db.relationship('Category', backref=db.backref('categories', lazy=True))
 
     image_1 = db.Column(db.String(150), nullable=False, default='image.jpg')
     image_2 = db.Column(db.String(150), nullable=False, default='image.jpg')
     image_3 = db.Column(db.String(150), nullable=False, default='image.jpg')
 
 class Brand(db.Model):
-    """docstring for Brand."""
+    """Defines a model for Brand."""
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(30), nullable=False, unique=True)
@@ -37,7 +37,7 @@ class Brand(db.Model):
 
 
 class Category(db.Model):
-    """docstring for Brand."""
+    """Defines a model for Brand."""
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(30), nullable=False, unique=True)
