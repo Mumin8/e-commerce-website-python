bf064cfad3bf61c3534c341b1a301f780402bc20
Mumin8 <alhassanmumin8@gmail.com>
2023-05-31 17:13:12 +0000

    Add product to database

--- myshop/shop/products/routes.py
+++ myshop/shop/products/routes.py
@@ -1,11 +1,15 @@
-from flask import redirect, render_template, url_for, flash, request
-from shop import db, app
-from .models import Brand, Category
+from flask import redirect, render_template, url_for, flash, request, session
+from shop import db, app, photos
+from .models import Brand, Category, Addproduct
 from .forms import Addproducts
+import secrets
 
 
 @app.route('/addbrand', methods=['GET', 'POST'])
 def addbrand():
+    if 'email' not in session:
+        flash(f'please login first', 'danger')
+        return redirect(url_for('login'))
     if request.method == 'POST':
         getbrand = request.form.get('brand')
         brand = Brand(name=getbrand)
@@ -20,6 +24,9 @@ def addbrand():
 
 @app.route('/addcategory', methods=['GET', 'POST'])
 def addcat():
+    if 'email' not in session:
+        flash(f'please login first', 'danger')
+        return redirect(url_for('login'))
     if request.method == 'POST':
         getcategory = request.form.get('category')
         category = Category(name=getcategory)
@@ -33,9 +40,32 @@ def addcat():
 
 @app.route('/addproduct', methods=['GET','POST'])
 def addproduct():
+    if 'email' not in session:
+        flash(f'please login first', 'danger')
+        return redirect(url_for('login'))
     brands = Brand().query.all()
     categories = Category.query.all()
     form = Addproducts(request.form)
+    if request.method == 'POST':
+        name = form.name.data
+        price = form.price.data
+        discount = form.discount.data
+        stock = form.stock.data
+        desc = form.description.data
+        color = form.color.data
+        brand = request.form.get('brand')
+        category = request.form.get('category')
+        image_1 = photos.save(request.files.get('image_1'), name=secrets.token_hex(10)+'.')
+        image_2 = photos.save(request.files.get('image_2'), name=secrets.token_hex(10)+'.')
+        image_3 = photos.save(request.files.get('image_3'), name=secrets.token_hex(10)+'.')
+
+        addprod = Addproduct(name=name, price=price, discount=discount, stock=stock, desc=desc,
+        color=color, brand_id=brand, category_id=category,image_1=image_1, image_2 = image_2,
+                             image_3=image_3 )
+        db.session .add(addprod)
+        flash("the product {} has been added to the database".format(name), 'success')
+        db.session.commit()
+        return redirect(url_for('admin'))
 
     return render_template('products/addproduct.html', title='Add PRODUCT page', form=form,
     brands= brands, categories=categories)
