9ff00ce6bc3e2b81fe6c6b87eb6643d03c1b53c9
Mumin8 <alhassanmumin8@gmail.com>
2023-07-13 12:27:39 +0000

    pep8 compliance and code documenting

--- myshop/shop/products/routes.py
+++ myshop/shop/products/routes.py
@@ -14,7 +14,7 @@ def addbrand():
         getbrand = request.form.get('brand')
         brand = Brand(name=getbrand)
         db.session.add(brand)
-        flash(f'the brand {getbrand} added ','success')
+        flash(f'the brand {getbrand} added ', 'success')
         db.session.commit()
         return redirect(url_for('addbrand'))
 
@@ -32,7 +32,10 @@ def updatebrand(id):
         flash(f'Your brand has been updated successfull', 'success')
         db.session.commit()
         return redirect(url_for('brands'))
-    return render_template('products/updatebrand.html', title='update brand page', updatebrand = updatebrand)
+    return render_template(
+                            'products/updatebrand.html',
+                            title='update brand page', updatebrand=updatebrand
+                            )
 
 
 @app.route('/addcategory', methods=['GET', 'POST'])
@@ -44,10 +47,11 @@ def addcat():
         getcategory = request.form.get('category')
         category = Category(name=getcategory)
         db.session.add(category)
-        flash(f'the brand {getcategory} added ','success')
+        flash(f'the brand {getcategory} added ', 'success')
         db.session.commit()
         return redirect(url_for('addcat'))
     return render_template('products/addbrand.html')
+
 
 @app.route('/updatecat/<int:id>', methods=['GET', 'POST'])
 def updatecat(id):
@@ -60,17 +64,28 @@ def updatecat(id):
         flash(f'Your category has been updated successfull', 'success')
         db.session.commit()
         return redirect(url_for('categories'))
-    return render_template('products/updatebrand.html', title='update category page', updatecat = updatecat)
+    return render_template(
+                        'products/updatebrand.html',
+                        title='update category page', updatecat=updatecat
+                        )
 
 
-@app.route('/addproduct', methods=['GET','POST'])
+# The end point to addproduct function
+@app.route('/addproduct', methods=['GET', 'POST'])
 def addproduct():
+    '''
+    addproduct:
+                this function adds a product to the database
+    '''
+
     if 'email' not in session:
         flash(f'please login first', 'danger')
         return redirect(url_for('login'))
     brands = Brand().query.all()
     categories = Category.query.all()
     form = Addproducts(request.form)
+
+    # Get the required data from the front end
     if request.method == 'POST':
         name = form.name.data
         price = form.price.data
@@ -80,17 +95,40 @@ def addproduct():
         color = form.color.data
         brand = request.form.get('brand')
         category = request.form.get('category')
-        image_1 = photos.save(request.files.get('image_1'), name=secrets.token_hex(10)+'.')
-        image_2 = photos.save(request.files.get('image_2'), name=secrets.token_hex(10)+'.')
-        image_3 = photos.save(request.files.get('image_3'), name=secrets.token_hex(10)+'.')
 
-        addprod = Addproduct(name=name, price=price, discount=discount, stock=stock, desc=desc,
-        color=color, brand_id=brand, category_id=category,image_1=image_1, image_2 = image_2,
-                             image_3=image_3 )
-        db.session .add(addprod)
-        flash("the product {} has been added to the database".format(name), 'success')
+        # upload the required images from your system
+        image_1 = photos.save(
+                            request.files.get('image_1'),
+                            name=secrets.token_hex(10)+'.'
+                            )
+        image_2 = photos.save(
+                            request.files.get('image_2'),
+                            name=secrets.token_hex(10)+'.'
+                            )
+        image_3 = photos.save(
+                                request.files.get('image_3'),
+                                name=secrets.token_hex(10)+'.'
+                                )
+
+        # Add the data to the models
+        addprod = Addproduct(
+                name=name, price=price, discount=discount, stock=stock,
+                desc=desc, color=color, brand_id=brand, category_id=category,
+                image_1=image_1, image_2 image_2, image_3=image_3
+                )
+
+        # add the data to session
+        db.session.add(addprod)
+        flash(
+            "the product {} has been added to the database".format(name),
+            'success'
+            )
+
+        # commit the data to the database
         db.session.commit()
         return redirect(url_for('admin'))
 
-    return render_template('products/addproduct.html', title='Add PRODUCT page', form=form,
-    brands= brands, categories=categories)
+    return render_template(
+            'products/addproduct.html', title='Add PRODUCT page',
+            form=form, brands=brands, categories=categories
+            )
