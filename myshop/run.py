876be520816fdb8d374047255acceabdee58672c
Mumin8 <alhassanmumin8@gmail.com>
2023-07-13 11:57:26 +0000

    pep compliance

--- myshop/run.py
+++ myshop/run.py
@@ -1,4 +1,8 @@
 from shop import app
 
-if __name__=="__main__":
+if __name__ == "__main__":
+    '''
+    This is the entry point for the application
+    '''
+    # start the running of the applicaion
     app.run(debug=True)
