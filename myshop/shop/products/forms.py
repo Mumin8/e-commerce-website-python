144d8f22a6818e0c2537035af7ea631f6718ce63
Mumin8 <alhassanmumin8@gmail.com>
2023-07-14 10:37:34 +0000

    refactor

--- myshop/shop/products/forms.py
+++ myshop/shop/products/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf.file import FileAllowed, FileField, FileRequired
-from wtforms import IntegerField, StringField, BooleanField, TextAreaField
+from wtforms import IntegerField, StringField, BooleanField, TextAreaField, DecimalField
 from wtforms.validators import DataRequired
 from flask_wtf import FlaskForm
 
@@ -8,12 +8,13 @@ class Addproducts(FlaskForm):
     """this is the class for the product model"""
 
     name = StringField('Name', validators=[DataRequired()])
-    price = IntegerField('Price', validators=[DataRequired()])
+    # if this field is IntegerField you will encounter integrity issue
+    price = DecimalField('Price', validators=[DataRequired()])
     discount = IntegerField('Discount', default=0)
     stock = IntegerField('Stock', validators=[DataRequired()])
     description = TextAreaField('Description', validators=[DataRequired()])
     color = TextAreaField('Color', validators=[DataRequired()])
 
-    image_1 = FileField('Image_1', validators= [FileRequired(), FileAllowed(['jpg', 'png', 'gif', 'jpeg'], 'Images only')])
-    image_2 = FileField('Image_2', validators= [FileRequired(), FileAllowed(['jpg', 'png', 'gif', 'jpeg'])])
-    image_3 = FileField('Image_3', validators= [FileRequired(), FileAllowed(['jpg', 'png', 'gif', 'jpeg'])])
+    image_1 = FileField('Image_1', validators= [FileAllowed(['jpg', 'png', 'gif', 'jpeg'])])
+    image_2 = FileField('Image_2', validators= [FileAllowed(['jpg', 'png', 'gif', 'jpeg'])])
+    image_3 = FileField('Image_3', validators= [FileAllowed(['jpg', 'png', 'gif', 'jpeg'])])
