54416855beb21ad962a4d7d6cd0b42e9470cf435
Mumin8 <alhassanmumin8@gmail.com>
2023-07-20 21:30:57 +0000

    the cart function

--- myshop/shop/carts/carts.py
+++ myshop/shop/carts/carts.py
@@ -11,22 +11,36 @@ def MergerDicts(dict1, dict2):
         return dict(list(dict1.items()) + list(dict2.items()))
     return False
 
+
+# the endpoint to the AddCart function
 @app.route('/addcart', methods=['POST'])
 def AddCart():
+    '''
+    AddCart:
+            the function to add cart data
+    '''
     try:
         product_id = request.form.get('product_id')
         quantity = request.form.get('quantity')
         colors = request.form.get('colors')
         product = Addproduct.query.filter_by(id=product_id).first()
         if product_id and quantity and colors and request.method == 'POST':
-            DictItems = {product_id:{'name': product.name, 'price': product.price, 'discount': product.discount,
-                                      'color': colors, 'quantity': quantity,'image': product.image_1}}
+            DictItems = {
+                        product_id: {
+                                'name': product.name, 'price': product.price,
+                                'discount': product.discount, 'color': colors,
+                                'quantity': quantity, 'image': product.image_1
+                                    }
+                        }
             if 'Shoppingcart' in session:
-                print(session['Shoppingcart'])
+                # print(session['Shoppingcart'])
                 if product_id in session['Shoppingcart']:
                     print(f'{product.name} already in shopping cart ')
                 else:
-                    session['Shoppingcart'] = MergerDicts(session['Shoppingcart'], DictItems)
+                    session['Shoppingcart'] = MergerDicts(
+                                                session['Shoppingcart'],
+                                                DictItems
+                                                )
                     return redirect(request.referrer)
             else:
                 session['Shoppingcart'] = DictItems
@@ -35,3 +49,27 @@ def AddCart():
         print(e)
     finally:
         return redirect(request.referrer)
+
+
+# the endpoint to the getCart function
+@app.route('/carts')
+def getCart():
+    '''
+    getCart:
+            the function to get the cart data
+    '''
+
+    if 'Shoppingcart' not in session:
+        return redirect(request.referrer)
+    subtotal = 0
+    grandtotal = 0
+    for key, product in session['Shoppingcart'].items():
+        discount = (product['discount']/100) * float(product['price'])
+        subtotal += float(product['price']) * int(product['quantity'])
+        subtotal -= discount
+        tax = ("%.2f" % (.06 * float(subtotal)))
+        grandtotal = float("%0.2f" % (1.06 * subtotal))
+    return render_template(
+                            'products/carts.html',
+                            tax=tax, grandtotal=grandtotal
+                            )
