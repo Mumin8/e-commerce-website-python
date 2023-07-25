bdec8867dfce33b5c162abb153127a6b347d431b
Mumin8 <alhassanmumin8@gmail.com>
2023-07-25 11:41:12 +0000

    register the route

--- myshop/shop/__init__.py
+++ myshop/shop/__init__.py
@@ -3,6 +3,7 @@ from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
 from flask_uploads import IMAGES, UploadSet, configure_uploads
 import os
+from flask_msearch import Search
 
 # Get the base directory
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -26,8 +27,11 @@ db.init_app(app)
 
 # instantiate Bcrypt with the flask app
 bcrypt = Bcrypt(app)
+search = Search()
+search.init_app(app)
 
 # import from routes from the packages
 from shop.admin import routes
 from shop.products import routes
 from shop.carts import carts
+from shop.customers import routes
