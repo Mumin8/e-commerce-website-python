8bc7095c436cd91983f5ee283169d3015551178c
Mumin8 <alhassanmumin8@gmail.com>
2023-07-26 13:32:28 +0000

    customer login and logout

--- myshop/shop/customers/routes.py
+++ myshop/shop/customers/routes.py
@@ -1,21 +1,43 @@
 from flask import redirect, render_template, url_for, flash
 from flask import request, session, current_app
-from shop import db, app, photos, search, bcrypt
-from .forms import CustomerRegistrationForm
+from shop import db, app, photos, search, bcrypt, login_manager
+from flask_login import login_required, current_user, logout_user, login_user
+from .forms import CustomerRegistrationForm, customerLoginForm
 from .model import Register
 import secrets
 import os
 
 @app.route('/customer/register', methods=['GET', 'POST'])
 def customer_register():
-    form = CustomerRegistrationForm(request.form)
-    if request.method == 'POST':
-        hash_password = bcrypt.generate_password_hash(form.password.data)
+    form = CustomerRegistrationForm()
+    if form.validate_on_submit():
+        hash_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
         register = Register(name=form.name.data, username=form.username.data, email=form.email.data,
-                            password=form.password.data, country=form.country.data, state=form.state.data, city=form.city.data,
+                            password=hash_password, country=form.country.data, city=form.city.data,
                             address=form.address.data, zipcode=form.zipcode.data)
         db.session.add(register)
         db.session.commit()
         flash(f'Welcome {form.name.data} Thank you for registering', 'success')
         return redirect(url_for('login'))
     return render_template('customer/register.html', form=form)
+
+
+@app.route('/customer/login', methods=['GET', 'POST'])
+def customerLogin():
+    form = customerLoginForm()
+    if form.validate_on_submit():
+        user = Register.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user)
+            flash(f'You are login', 'success')
+            next = request.args.get('next')
+            return redirect(next or url_for('home'))
+        flash('Incorrect username and password combination', 'danger')
+        return redirect(url_for('customerLogin'))
+    return render_template('customer/login.html', form=form)
+
+
+@app.route('/customer/logout')
+def customer_logout():
+    logout_user()
+    return redirect(url_for('customerLogin'))
