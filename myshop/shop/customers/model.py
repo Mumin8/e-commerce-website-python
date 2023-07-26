b923b169d9adf77683ed6ec99e7d75408b23f7a0
Mumin8 <alhassanmumin8@gmail.com>
2023-07-26 13:31:32 +0000

    refactor

--- myshop/shop/customers/model.py
+++ myshop/shop/customers/model.py
@@ -1,16 +1,21 @@
-from shop import db, app
+from shop import db, app, login_manager
+from flask_login import UserMixin
 from datetime import datetime
 
+@login_manager.user_loader
+def user_loader(user_id):
+    return Register.query.get(user_id)
 
-class Register(db.Model):
+
+class Register(db.Model,  UserMixin):
     """docstring for Register."""
     id = db.Column(db.Integer, primary_key= True)
     name = db.Column(db.String(60), unique= False)
     username = db.Column(db.String(80), unique= True)
-    email = db.Column(db.String(60), unique= True)
+    email = db.Column(db.String(80), unique= True)
     password = db.Column(db.String(180), unique= False)
     country = db.Column(db.String(60), unique= False)
-    state = db.Column(db.String(60), unique= False)
+    # state = db.Column(db.String(60), unique= False)
     city = db.Column(db.String(60), unique= False)
     contact = db.Column(db.String(60), unique= False)
     address = db.Column(db.String(60), unique= False)
