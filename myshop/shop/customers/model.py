536f0b7d2d886819390b92952053701797951f19
Mumin8 <alhassanmumin8@gmail.com>
2023-07-27 00:40:03 +0000

    order model

--- myshop/shop/customers/model.py
+++ myshop/shop/customers/model.py
@@ -1,6 +1,7 @@
 from shop import db, app, login_manager
 from flask_login import UserMixin
 from datetime import datetime
+import json
 
 @login_manager.user_loader
 def user_loader(user_id):
@@ -25,5 +26,34 @@ class Register(db.Model,  UserMixin):
 
     def __repre__(self):
         return '<Register %r> % self.name'
+
+class JsonEncodedDict(db.TypeDecorator):
+    """docstring for JsonEncodedDict."""
+    impl = db.Text
+
+    def process_bind_param(self, value, dialet):
+        if value is None:
+            return '{}'
+        else:
+            return json.dumps(value)
+
+    def process_result_param(self, value, dialet):
+        if value is None:
+            return '{}'
+        else:
+            return json.loads(value)
+
+class CustomerOrder(db.Model):
+    """docstring for CustomerOrder."""
+    id = db.Column(db.Integer, primary_key=True)
+    invoice = db.Column(db.String(20), unique= True, nullable=False)
+    status = db.Column(db.String(20),default='pending', nullable=False)
+    customer_id = db.Column(db.Integer, unique= False, nullable=False)
+    date_created = db.Column(db.DateTime, nullable= False, default=datetime.utcnow)
+    orders = db.Column(JsonEncodedDict)
+
+    def __repr__(self):
+        return '<CustomerOrder %r> % self.invoice'
+
 with app.app_context():
     db.create_all()
